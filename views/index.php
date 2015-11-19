d4024fd9a3d1f04623c0de498e0230efdf824ee4
Mario Aquino <marioaquino31@hotmail.com>
2015-11-19 17:32:18 -0200

    CreateHomeController

--- views/index.php
+++ views/index.php
@@ -1,8 +1,11 @@
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+<h1 class="h2" id="title">Home</h1> 
+<hr/>
+<ul class="list-unstyled list-group">
+    <?php
+    $ths = '';
+    foreach ($menu as $key => $value) :
+        $ths .= "<li class='list-group-item'><b>" . ucfirst($key) . ":</b> {$value}</li>";
+    endforeach;
+    echo $ths;
+    ?>
+</ul>
